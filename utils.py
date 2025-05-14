e6d10c0fb3dedb573d4655c1f79e417a82ce989a
Adalberto Machin <70716647+Adalberto-Machin@users.noreply.github.com>
2025-05-14 08:39:01 -0500

    started the report and created the utils file for creating the class that will do the analysis'

---
+++ utils.py
@@ -0,0 +1,19 @@
+# this is the python util file where I will create my different classes
+
+# import all necessary libraries
+
+
+# create a class for the training of the different models with the structure below:
+
+# 1 Main class where the model data is loaded appropriately
+    # 1a Method of the main class where the model is tokenized
+#2 Sub-Class where we do the training of the unsupervised model
+    # 2a Method where any ensemble methods, improvements are made to the model training
+#3 Method where we predict the test data and measure accuracy
+#4 Sub-class where we do a training of the supervised model
+    # 4a Method where we improve training of the supervised model
+# eventually, the supervised model should use the same method we defined for the prediction in order to test its accuracy
+
+
+class articles():
+    def
